eb2dfb52ee8609adbffde298e76fe9e736d26216
Leonardo Cantanhêde <leocantanhede@gmail.com>
2022-11-05 13:23:00 +0000

    Contiuação Swagger

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -17,43 +17,91 @@ import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonService;
 import br.com.erudio.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("api/person/v1")
+@Tag(name = "People", description = "Endpoint for Managing People")
 public class PersonController {
 
 	@Autowired
 	private PersonService service;
-	
+
 	@PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML}, produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Adds a new Person", description = "Adds a new Person by passing in a JSON, XML or YML representation of the person!", tags = {"People"},
+	responses = {
+		@ApiResponse(description = "Success", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonVO.class))),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Foud", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+	})
 	public PersonVO create(@RequestBody PersonVO person) {
 		return this.service.create(person);
 	}
-	
+
 	@PostMapping(value = "/v2", consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML}, produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
 		return this.service.createV2(person);
 	}
-	
+
 	@PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML}, produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Updates a Person", description = "Updates a Person by passing in a JSON, XML or YML representation of the person!", tags = {"People"},
+	responses = {
+		@ApiResponse(description = "Updated", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonVO.class))),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Foud", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+	})
 	public PersonVO update(@RequestBody PersonVO person) {
 		return this.service.update(person);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Deletes a Person", description = "Deletes a Person by passing in a JSON, XML or YML representation of the person!", tags = {"People"},
+	responses = {
+		@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Foud", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+	})
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		this.service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Finds a Person", description = "Finds a Person", tags = {"People"},
+	responses = {
+		@ApiResponse(description = "Success", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonVO.class))),
+		@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+		@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+		@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+		@ApiResponse(description = "Not Foud", responseCode = "404", content = @Content),
+		@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+	})
 	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return this.service.findById(id);
 	}
-	
+
 	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Finds all People", description = "Finds all People", tags = {"People"},
+		responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))}),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Foud", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+	})
 	public List<PersonVO> findAll() {
 		return this.service.findAll();
 	}
-	
+
 }
